a7bd5133f5842bbadb79edf8b7c89beae955975a
Ivan Lott <ivanlott@hotmail.com>
2023-05-08 22:18:53 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express'); //chamando o express
+const app = express(); //instanciando e guardando o express na variável app
+
+app.set("view engine", "ejs"); // está falando para o express, que a ferramente que vai ser utilizada para renderizar html, vai ser utilizado o ejs
+
+app.get("/",function(req, res){
+    res.render("index"); //app.get está criando uma rota, a / refere-se ao nome do site
+});
+
+app.get("/sobre",function(req, res){ // é o nome do site/sobre
+    res.render("about");
+});
+
+app.listen(8080); //carregando tudo do express, com a função listen q irá ouvir a porta 8080
+console.log('rodando');
